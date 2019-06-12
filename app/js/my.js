207fe18e0cbac533bca3c3efa431957c9529a62e
Kanunnikov Valentin <89417@mail.ru>
2019-06-12 15:58:09 +0300

    ready for color palette

--- app/js/my.js
+++ app/js/my.js
@@ -1,7 +1,34 @@
 // Пракрутка вверх
-$("#back-top").click(function (){
+$("#back-top").click(function () {
     $("body,html").animate({
-        scrollTop:0
+        scrollTop: 0
     }, 800);
     return false;
+});
+
+/* gallery */
+$(".filter-button").click(function () {
+    var value = $(this).attr('data-filter');
+
+    if (value == "all") {
+        //$('.filter').removeClass('hidden');
+        $('.filter').show('1000');
+    }
+    else {
+        //            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
+        //            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
+        $(".filter").not('.' + value).hide('3000');
+        $('.filter').filter('.' + value).show('3000');
+
+    }
+});
+
+if ($(".filter-button").removeClass("active")) {
+    $(this).removeClass("active");
+}
+$(this).addClass("active");
+
+// Скрипт для Палитры цветов натяжных потолков
+$('.color-palette__btn ').click(function() {
+    $('.color-palette__img').attr("src", $(this).attr('img-color-src'))
 });
